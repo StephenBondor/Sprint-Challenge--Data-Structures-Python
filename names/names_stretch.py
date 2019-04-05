4c9fbc1587e43def927bf171f3c1fff18021b17c
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-04-05 09:32:41 -0700

    BST search added

---
+++ names/names_stretch.py
@@ -0,0 +1,25 @@
+import time
+from collections import Counter
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+c_names_1 = Counter(names_1)
+c_names_2 = Counter(names_2)
+
+
+duple = c_names_1 & c_names_2
+duplicates = duple.elements()
+
+print(duplicates)
+
+end_time = time.time()
+# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print(f"runtime: {end_time - start_time} seconds")
